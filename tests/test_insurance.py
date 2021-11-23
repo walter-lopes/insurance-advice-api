32faa1cb8993286b4faca4595c489cf39179a715
walter-lopes <walter_vinicius@outlook.com>
2021-11-23 12:20:52 -0300

    fixing tests

--- tests/test_insurance.py
+++ tests/test_insurance.py
@@ -1,5 +1,5 @@
 import unittest
-
+import datetime
 from src.domain.insurance import Insurance
 from src.domain.user import User
 from src.domain.score import Score
@@ -23,7 +23,8 @@ class Insurance_Test(unittest.TestCase):
         self.assertEqual(score['auto'], Score.ineligible.name)
 
     def test_should_apply_for_user_variable(self):
-        user = User(29, 1, 'mortgaged', 199999, 'married', [0, 1, 1], 2020)
+        year = datetime.datetime.now().year - 1
+        user = User(29, 1, 'mortgaged', 199999, 'married', [0, 1, 1], year)
         insurance = Insurance(user)
         score = insurance.get_risk_score()
         self.assertEqual(score['disability'], Score.economic.name)
@@ -31,8 +32,9 @@ class Insurance_Test(unittest.TestCase):
         self.assertEqual(score['auto'], Score.economic.name)
         self.assertEqual(score['life'], Score.economic.name)
 
-    def test_should_apply_for_user_variable(self):
-        user = User(29, 1, 'mortgaged', 199999, 'married', [0, 1, 1], 2020)
+    def test_should_apply_for_user_variables_case_1(self):
+        year = datetime.datetime.now().year - 1
+        user = User(29, 1, 'mortgaged', 199999, 'married', [0, 1, 1], year)
         insurance = Insurance(user)
         score = insurance.get_risk_score()
         self.assertEqual(score['disability'], Score.economic.name)
@@ -40,8 +42,9 @@ class Insurance_Test(unittest.TestCase):
         self.assertEqual(score['auto'], Score.economic.name)
         self.assertEqual(score['life'], Score.economic.name)
 
-    def test_should_apply_for_user_variable_2(self):
-        user = User(30, 1, 'mortgaged', 199999, 'married', [0, 1, 1], 2020)
+    def test_should_apply_for_user_variables_case_2(self):
+        year = datetime.datetime.now().year - 1
+        user = User(30, 1, 'mortgaged', 199999, 'married', [0, 1, 1], year)
         insurance = Insurance(user)
         score = insurance.get_risk_score()
         self.assertEqual(score['disability'], Score.economic.name)
@@ -49,8 +52,9 @@ class Insurance_Test(unittest.TestCase):
         self.assertEqual(score['auto'], Score.economic.name)
         self.assertEqual(score['life'], Score.regular.name)
 
-    def test_should_apply_for_user_variable_2(self):
-        user = User(35, 1, 'mortgaged', 199999, 'married', [0, 1, 1], 2020)
+    def test_should_apply_for_user_variables_case_3(self):
+        year = datetime.datetime.now().year - 4
+        user = User(35, 1, 'mortgaged', 199999, 'married', [0, 1, 1], year)
         insurance = Insurance(user)
         score = insurance.get_risk_score()
         self.assertEqual(score['disability'], Score.economic.name)
@@ -58,8 +62,9 @@ class Insurance_Test(unittest.TestCase):
         self.assertEqual(score['auto'], Score.economic.name)
         self.assertEqual(score['life'], Score.regular.name)
 
-    def test_should_apply_for_user_variable_2(self):
-        user = User(40, 1, 'mortgaged', 199999, 'married', [0, 1, 1], 2020)
+    def test_should_apply_for_user_variables_case_4(self):
+        year = datetime.datetime.now().year - 4
+        user = User(40, 1, 'mortgaged', 199999, 'married', [0, 1, 1], year)
         insurance = Insurance(user)
         score = insurance.get_risk_score()
         self.assertEqual(score['disability'], Score.economic.name)
@@ -67,8 +72,9 @@ class Insurance_Test(unittest.TestCase):
         self.assertEqual(score['auto'], Score.economic.name)
         self.assertEqual(score['life'], Score.regular.name)
 
-    def test_should_apply_for_user_variable_3(self):
-        user = User(59, 1, 'mortgaged', 199999, 'married', [0, 1, 1], 2020)
+    def test_should_apply_for_user_variables_case_5(self):
+        year = datetime.datetime.now().year - 1
+        user = User(59, 1, 'mortgaged', 199999, 'married', [0, 1, 1], year)
         insurance = Insurance(user)
         score = insurance.get_risk_score()
         self.assertEqual(score['disability'], Score.regular.name)
@@ -76,7 +82,7 @@ class Insurance_Test(unittest.TestCase):
         self.assertEqual(score['auto'], Score.regular.name)
         self.assertEqual(score['life'], Score.regular.name)
 
-    def test_should_apply_for_user_variable_4(self):
+    def test_should_apply_for_user_variables_case_6(self):
         user = User(59, 1, 'owned', 2000001, 'married', [0, 1, 1], 1999)
         insurance = Insurance(user)
         score = insurance.get_risk_score()
@@ -85,7 +91,7 @@ class Insurance_Test(unittest.TestCase):
         self.assertEqual(score['auto'], Score.economic.name)
         self.assertEqual(score['life'], Score.regular.name)
 
-    def test_should_apply_for_user_variable_5(self):
+    def test_should_apply_for_user_variables_case_7(self):
         user = User(59, 1, 'owned', 199999, 'single', [0, 1, 1], 1999)
         insurance = Insurance(user)
         score = insurance.get_risk_score()
